db10dc4ca9a5f5a9b84603cf45588ddedff7020d
Trikkeri <trikkeri@outlook.com>
2016-07-25 16:17:39 +0300

    Lisätty logiikka, jossa arvattava sana vaihdetaan toiseksi samankaltaiseksi sanaksi kun käyttäjä tekee oikean arvauksen (arvattu kirjain löytyy arvattavasta sanasta)

--- viikko13-Viikko13_206.Hirsipuu/src/hirsipuu/Hirsipuu.java
+++ viikko13-Viikko13_206.Hirsipuu/src/hirsipuu/Hirsipuu.java
@@ -3,6 +3,7 @@ package hirsipuu;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 public class Hirsipuu {
 
@@ -33,6 +34,7 @@ public class Hirsipuu {
         this.tehdytArvaukset.add(kirjain);
         
         if(this.arvattava.contains(String.valueOf(kirjain))) {
+            vaihdaArvattavaSana();
             return true;
         } else {
             this.arvauksiaJaljella -= 1;
@@ -42,7 +44,6 @@ public class Hirsipuu {
 
     
     public String sana() {
-        
         char[] arvattavaSana = this.arvattava.toCharArray();
         StringBuilder sb = new StringBuilder();
         
@@ -66,5 +67,32 @@ public class Hirsipuu {
             return true;
         }
         return false;
+    }
+
+    private void vaihdaArvattavaSana() {
+        System.out.println("Vanha sana ennen vaihtoa: " + this.arvattava);
+        System.out.println("Välitetty sana: " + this.sana());
+        
+        Sanalista uudetSanat = this.sanalista.sanatJoissaMerkit(this.sana());
+        String uusiArvattavaSana = "";
+        Random rnd = new Random();
+        
+//        System.out.println("UUDET SANAT: ");
+//        for(String sana : uudetSanat.sanat()) {
+//            System.out.println(sana);
+//        }
+        
+        Collections.shuffle(uudetSanat.sanat());
+        this.arvattava = uudetSanat.sanat().get(0);
+        
+        // Valitaan toisen saman pituinen ja samankaltainen sana varmistaen ettei valita jo muuttujassa olevaa sanaa jos sanalistassa on enemmän kuin yksi sana
+        
+        if(uudetSanat.sanat().size() > 1) {
+            while(this.arvattava.equalsIgnoreCase(uusiArvattavaSana)) {
+                this.arvattava = uudetSanat.sanat().get(rnd.nextInt(uudetSanat.sanat().size() - 1));
+            }    
+        }
+
+        System.out.println("Uusi sana vaihdon jälkeen: " + this.arvattava);
     }
 }
